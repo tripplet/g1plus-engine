924c4e05ecdc8de8383912329d6639045743d6e8
Tobias Tangemann <tobiastangemann@gmail.com>
2012-04-27 14:08:47 +0200

    general code progress

---
+++ cron.py
@@ -0,0 +1,97 @@
+#!/usr/bin/env python
+
+# Imports ###########################################################################
+# ###################################################################################
+
+import webapp2
+import urllib
+import hashlib
+import re
+import DataModel
+import feedparser
+
+from datetime import date
+from bs4 import BeautifulSoup
+
+# Constants
+feed_encoding = 'ISO-8859-1'
+
+main_feed_url = 'http://feeds.feedburner.com/gameone'
+site_urls     = [('http://www.gameone.de/',      'img_link'),
+                 ('http://www.gameone.de/blog/', 'image_link')]
+
+regex_links = '/blog/([0-9]{4})/([0-9]{1,2})/([a-zA-Z0-9_-]+)/?'
+
+# Cronjob handler ###################################################################
+# ###################################################################################
+class CronjobHandler(webapp2.RequestHandler):
+
+    # crawl an RSS feed for new entries
+    @staticmethod
+    def crawlRssFeed(feed_url, type):
+      # prase feed
+      d = feedparser.parse(feed_url)
+
+      for entry in d.entries:
+        try:
+          # get name and url
+          name = entry.title.encode(feed_encoding)
+          url = entry.link.encode(feed_encoding)
+
+          # skip if feed entry already existent -  based on etnry name, which should!! be unique
+          if not DataModel.FeedEntry.all().filter('name =', name).get() or type == 'unrestricted':
+            new_entry = DataModel.FeedEntry(name=name, url=url)
+
+            # save page content based on cronjob type for later comparison
+            if type == 'restricted':
+              new_entry.restricted = urllib.urlopen(r).read()
+            elif type == 'unrestricted':
+              new_entry.unrestricted = urllib.urlopen(url).read()
+
+            new_entry.put()
+        except:
+          pass
+
+
+
+    # crawl a Page and extract specific links using html parsing engine "BeautifulSoup"
+    @staticmethod
+    def crawlPage(page_url, link_class, type):
+      source_code = urllib.urlopen(page_url).read()
+      soup = BeautifulSoup(source_code)
+
+      # dates to filter out old links during page crawl
+      cur_year = date.today().year
+      cur_month = date.today().month
+
+      # get all links tagged with class: $link_class
+      for item in soup.find_all('a', { "class" : link_class }):
+        x = re.search(regex_links,item.get('href'))
+
+        if x == None:
+          continue
+        else:
+          if (int(x.group(1)) == cur_year and int(x.group(2)) >= cur_month-1) or \
+              cur_month == 1 and (int(x.group(2)) == 12 and int(x.group(1)) == cur_year-1):
+            pass #print(x.group(1) + '/' + x.group(2) + '/' + x.group(3))
+            # todo
+
+
+
+    # handle web (get) requestand perform
+    def get(self, restricted_type):
+        # fetch rss feed for new entries
+        self.crawlRssFeed(main_feed_url, restricted_type)
+
+        # fetch sites for brand new posts which are not yet in the rss feed
+        #for site in site_urls:
+          #self.crawlPage(site[0], site[1], restricted_type)
+
+        self.response.out.write('Done!')
+
+
+# Main handler ######################################################################
+# ###################################################################################
+app = webapp2.WSGIApplication([('/cron/crawl/(\w+)', CronjobHandler)], debug=True)
+# ###################################################################################
+# ###################################################################################
--- main.py
+++ main.py
@@ -1,9 +1,12 @@
 #!/usr/bin/env python
 
-
 import webapp2
 
+import urllib
 import hashlib
+import re
+from datetime import date
+from bs4 import BeautifulSoup
 
 import DataModel
 
@@ -11,22 +14,72 @@ import feedparser
 
 feed_encoding = 'ISO-8859-1'
 
+feed_url = 'http://feeds.feedburner.com/gameone'
+main_site_url = 'http://www.gameone.de/'
+blog_site_url = 'http://www.gameone.de/blog/'
+
+main_link_find = '/blog/([0-9]{4})/([0-9]{1,2})/([a-zA-Z0-9_-]+)/?'
+blog_link_find = 'href="([0-9]{4})/([0-9]{1,2})/([a-zA-Z0-9_-]+)/?"'
+
+cur_year = date.today().year
+cur_month = date.today().month
+
+def printline(obj, data):
+  obj.response.out.write(data + '<br>')
+
 class MainHandler(webapp2.RequestHandler):
     def get(self):
         self.response.out.write('Hello world!')
-        DataModel.BlogPost(url='http://google.de',name='test').put()
 
 class TestHandler(webapp2.RequestHandler):
     def get(self):
-        d = feedparser.parse("http://feeds.feedburner.com/gameone")
+        d = feedparser.parse(feed_url)
         for x in d.entries:
           try:
-            self.response.out.write(x.title.encode(feed_encoding))
-            self.response.out.write(x.link.encode(feed_encoding))
-          except:
-            self.response.out.write('Error at: ' + repr(x.title))
+            name = x.title.encode(feed_encoding)
+            url = x.link.encode(feed_encoding)
 
-        self.response.out.write('success!')
+            if not DataModel.FeedEntry.all().filter('name =', name).get():
+              new_entry = DataModel.FeedEntry(name=name, url=url)
+              new_entry.restricted = urllib.urlopen(url).read()
+              new_entry.put()
+
+              printline(self, name)
+          except:
+            pass
+
+        printline(self, '<br>success!')
+
+class BlogLoaderHandler(webapp2.RequestHandler):
+    def get(self):
+      source_code = urllib.urlopen(blog_site_url).read()
+      soup = BeautifulSoup(source_code)
+      for item in soup.find_all('a', { "class" : "image_link" }):
+        self.response.out.write(item.get('href'))
+        self.response.out.write('<br>')
+
+
+      self.response.out.write('<br>')
+      self.response.out.write('<br>')
+
+
+      source_code = urllib.urlopen(main_site_url).read()
+      soup = BeautifulSoup(source_code)
+      for item in soup.find_all('a', { "class" : "img_link" }):
+        x = re.search(main_link_find,item.get('href'))
+
+        if x == None:
+          continue
+        else:
+          if (int(x.group(1)) == cur_year and int(x.group(2)) >= cur_month-1) or \
+              cur_month == 1 and (int(x.group(2)) == 12 and int(x.group(1)) == cur_year-1):
+            printline(self, 'http://www.gameone.de/blog/' + x.group(1) + '/' + x.group(2) + '/' + x.group(3))
+
+class ParseHandler(webapp2.RequestHandler):
+    def get(self):
+      self.response.out.write('test')
 
 app = webapp2.WSGIApplication([('/', MainHandler),
-                               ('/test', TestHandler)], debug=True)
+                               ('/test', TestHandler),
+                               ('/url', BlogLoaderHandler),
+                               ('/parse', ParseHandler)], debug=True)

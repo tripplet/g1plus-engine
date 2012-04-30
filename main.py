f696609c6ed95cb5c4de58345b0ef75cbf5f28f3
Tobias Tangemann <tobiastangemann@gmail.com>
2012-04-30 17:01:38 +0200

    jinja2 template engine list of datastore elements

--- main.py
+++ main.py
@@ -1,9 +1,32 @@
 #!/usr/bin/env python
 
 import webapp2
+import jinja2
+import os
+import hashlib
+import DataModel
+
+jinja_environment = jinja2.Environment(loader=jinja2.FileSystemLoader(os.path.dirname(__file__)))
 
 class MainHandler(webapp2.RequestHandler):
     def get(self):
-        self.response.out.write('Hello world!')
 
-app = webapp2.WSGIApplication([('/', MainHandler)], debug=True)
+      template_values = {
+        'entries': DataModel.FeedEntry.all()
+      }
+
+      template = jinja_environment.get_template('main_template.html')
+      self.response.out.write(template.render(template_values))
+
+class BinaryDataHandler(webapp2.RequestHandler):
+    def get(self, key, version):
+      entry = DataModel.FeedEntry.get_by_id(int(key))
+
+      if version == 'res':
+        self.response.out.write(entry.restricted)
+      elif version == 'unres':
+        self.response.out.write(entry.unrestricted)
+
+
+app = webapp2.WSGIApplication([('/', MainHandler),
+                               ('/(\w+)_(\w+)', BinaryDataHandler)], debug=True)
